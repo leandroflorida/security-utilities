8bffddc970abd2043b52d295469cb390cc111c39
Mary Martin <marymart@microsoft.com>
2022-01-14 17:38:16 -0800

    Using statement formatting and copyright headers added

--- src/Tests.Microsoft.Security.Utilities/IdentifiableSecretsTests.cs
+++ src/Tests.Microsoft.Security.Utilities/IdentifiableSecretsTests.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using System.Linq;
 using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Security.Utilities
 {
